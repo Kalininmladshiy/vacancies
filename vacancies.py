9031a98b369643d65752e779eefd13777327d8f0
Максим Калинин <maksim.kalinin1@gmail.com>
2022-08-31 07:06:41 +0300

    Formatted the code

--- vacancies.py
+++ vacancies.py
@@ -8,27 +8,32 @@ from dotenv import load_dotenv
 def predict_rub_salary_hh(vacancie):
     if vacancie['currency'] == 'RUR':
         if vacancie['from'] and vacancie['to']:
-            return round(mean([vacancie['from'], vacancie['to']]), 2)
+            return int(mean([vacancie['from'], vacancie['to']]))
         elif not vacancie['from']:
-            return round(vacancie['to'] * 0.8, 2)
+            return int(vacancie['to'] * 0.8)
         elif not vacancie['to']:
-            return round(vacancie['from'] * 1.2, 2)
-        
+            return int(vacancie['from'] * 1.2)
+
 
 def predict_rub_salary_sj(vacancie):
     if vacancie['currency'] == 'rub':
         if vacancie['payment_from'] and vacancie['payment_to']:
-            return round(mean([vacancie['payment_from'], vacancie['payment_to']]), 2)
+            return int(mean([vacancie['payment_from'], vacancie['payment_to']]))
         elif not vacancie['payment_from']:
-            return round(vacancie['payment_to'] * 0.8, 2)
+            return int(vacancie['payment_to'] * 0.8)
         elif not vacancie['payment_to']:
-            return round(vacancie['payment_from'] * 1.2, 2)   
-        
-        
+            return int(vacancie['payment_from'] * 1.2)
+
+
 def get_beautiful_table(table_data, big_title):
     table = [
-        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'], 
-     ]   
+        [
+            'Язык программирования',
+            'Вакансий найдено',
+            'Вакансий обработано',
+            'Средняя зарплата',
+            ],
+     ]
     values = []
     for language, statistics in table_data.items():
         values.append(language)
@@ -38,13 +43,13 @@ def get_beautiful_table(table_data, big_title):
         values = []
     table = AsciiTable(table, big_title)
     return table.table
-        
+
 
 def get_avg_salary_hh(languages):
     url_hh = 'https://api.hh.ru/vacancies'
     headers_hh = {
         'User-Agent': 'HH-User-Agent',
-        }    
+        }
     avg_language_salary = {}
     for language in languages:
         salarys = []
@@ -59,7 +64,7 @@ def get_avg_salary_hh(languages):
                 'only_with_salary': 'true',
                 'currency': 'RUR',
                 'page': page
-                }        
+                }
             response = requests.get(url_hh, headers=headers_hh, params=payload)
             response.raise_for_status()
             for vacancie in response.json()['items']:
@@ -70,8 +75,8 @@ def get_avg_salary_hh(languages):
         avg_language_salary[language] = {
             "vacancies_found": response.json()['found'],
             "vacancies_processed": len(salarys),
-            "average_salary": round(mean(salarys), 0),
-        }    
+            "average_salary": int(mean(salarys)),
+        }
     return avg_language_salary
 
 
@@ -94,25 +99,25 @@ def get_avg_salary_sj(languages, secret_key):
                 'keyword': language,
                 'count': 100,
                 'page': page
-                }        
+                }
             response = requests.get(url_sj, headers=headers_sj, params=payload)
             response.raise_for_status()
             for vacancie_sj in response.json()['objects']:
                 if predict_rub_salary_sj(vacancie_sj):
                     salarys.append(predict_rub_salary_sj(vacancie_sj))
-            vacancies_found += len(response.json()['objects']) 
+            vacancies_found += len(response.json()['objects'])
             page += 1
         avg_language_salary[language] = {
             "vacancies_found": vacancies_found,
             "vacancies_processed": len(salarys),
-            "average_salary": round(mean(salarys), 0),
-        }    
+            "average_salary": int(mean(salarys)),
+        }
     return avg_language_salary
 
 
 if __name__ == '__main__':
     load_dotenv()
-    secret_key = os.environ['SJ_API_KEY']    
+    secret_key = os.environ['SJ_API_KEY']
     languages = [
         'JavaScript',
         'Java',
@@ -124,6 +129,13 @@ if __name__ == '__main__':
         'C',
         'Go',
      ]
-    print(get_beautiful_table(get_avg_salary_hh(languages), 'HeadHunter Moscow'))
-    print(get_beautiful_table(get_avg_salary_sj(languages, secret_key), 'SuperJob Moscow'))
-    
+    print(get_beautiful_table(
+        get_avg_salary_hh(languages),
+        'HeadHunter Moscow',
+     )
+          )
+    print(get_beautiful_table(
+        get_avg_salary_sj(languages, secret_key),
+        'SuperJob Moscow'
+     )
+          )
